42c7e3ed4c50a23b7438dbb7f766b731048e5f8b
developerHub01 <developerhubytchannel@gmail.com>
2024-05-16 21:43:25 +0600

    delete post complete

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -1,3 +1,12 @@
 <?php
+include "../constants/config.php";
+
+$post_id = $_GET['post_id'];
+
+$sql = "DELETE FROM post WHERE post_id = {$post_id};";
+
+$result = $conn->query($sql) or die("Query failed");
+
+if($result) header("Location: {$host_name}/admin/post.php");
 
 ?>
