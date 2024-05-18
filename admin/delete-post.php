098d4d3d65c8af6729d77eb99a8cdf860350696c
developerHub01 <developerhubytchannel@gmail.com>
2024-05-18 14:45:27 +0600

    remove category count while deleting

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -18,12 +18,21 @@ $result = $result->fetch_assoc();
 
 if($result['post_img']) unlink("upload/{$result['post_img']}");
 
+$post_category = "SELECT category FROM post WHERE post_id = {$post_id}";
+$post_category = $conn->query($post_category) or die("Query Failed");
+
+$post_category = $post_category->fetch_assoc()['category'];
+
 if($user_role) $sql = "DELETE FROM post WHERE post_id = {$post_id};";
 else $sql = "DELETE FROM post WHERE post_id = {$post_id} AND author = '{$user_id}';";
 
 $result = $conn->query($sql) or die("Query failed");
 
-if($result) header("Location: {$host_name}/admin/post.php");
+if($result) {
+  $sql = "UPDATE category SET post = post - 1 WHERE category_id = {$post_category}";
+  $result = $conn->query($sql) or die("Query failed");
+  
+  header("Location: {$host_name}/admin/post.php");
+}
 else echo "<h1>This is not your post.</h1>";
-
 ?>
